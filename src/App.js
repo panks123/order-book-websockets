28b6b7e1b476400990633b2d500366f79e252263
Pankaj <pankajkumarhzb787@gmail.com>
2023-01-26 17:49:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import OrderBook from "./OrderBook";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Crypto Order Book </h2>
+      <OrderBook />
     </div>
   );
-}
+};
 
 export default App;
